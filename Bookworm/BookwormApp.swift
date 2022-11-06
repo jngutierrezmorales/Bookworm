f66e189361cd729c5af79cac103ffba896dbe0f9
Juan Gutierrez <juan.gutierrez@auro-group.com>
2022-11-06 10:57:26 +0100

    Initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Juan Gutierrez on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
